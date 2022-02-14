4b8de90a055fcb74159a4d2c4c655024d6c88f51
Himanshu <mailboxofhimanshu1999@gmail.com>
2022-02-15 01:04:03 +0530

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    'mongodb+srv://himanshu:0wPBBqxY5tGFYaFk@cluster0.qxbn1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  SECRET_KEY: 'some very secret key'
+};
